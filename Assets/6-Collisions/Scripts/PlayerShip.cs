95c1d85d4bb8a857e87f2145dba85538612115d4
BirdyXV <BirdyDBZ@hotmail.com>
2017-08-17 16:10:30 +1000

    added untracked files

---
+++ Assets/6-Collisions/Scripts/PlayerShip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collisions
+{
+
+    public class PlayerShip : MonoBehaviour
+    {
+        public float acceleration = 20f;
+        public float rotationSpeed = 360f;
+        public float gottaGoFast = 150f;
+
+        private Rigidbody2D rigid;
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            // Physics.OverlapSphereAll();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+        }
+
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            float currentSpeed = acceleration;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = gottaGoFast;
+            }
+            rigid.AddForce(transform.up * inputV * acceleration);
+        }
+
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            // Vector3.back (...from outer space!), angle
+            transform.Rotate(Vector3.back, rotationSpeed * inputH * Time.deltaTime);
+        }
+    }
+}
